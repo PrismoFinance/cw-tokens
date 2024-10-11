e153dfb24988ec1d46ab1e7f84196ce039a346e8
PrismoFinance <moleculardot@proton.me>
2024-10-11 10:32:33 -0400

    Update msg.rs

--- contracts/cw20-escrow/src/msg.rs
+++ contracts/cw20-escrow/src/msg.rs
@@ -3,6 +3,7 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::{Addr, Api, Coin, StdResult};
 
 use cw20::{Cw20Coin, Cw20ReceiveMsg};
+use state::{Escrow, EscrowStatus};
 
 #[cw_serde]
 pub struct InstantiateMsg {}
@@ -32,6 +33,56 @@ pub enum ExecuteMsg {
         /// id is a human-readable name for the escrow from create
         id: String,
     },
+    UpdateEscrow {
+    pub status: EscrowStatus,
+         /// id is a human-readable name for the escrow to use later
+    /// 3-20 bytes of utf-8 text
+    pub id: String,
+    /// arbiter can decide to approve or refund the escrow
+    pub arbiter: String,
+    /// if approved, funds go to the recipient
+    pub recipient: Option<String>,
+    /// Title of the escrow
+    pub title: String,
+    /// Longer description of the escrow, e.g. what conditions should be met
+    pub description: String,
+    /// When end height set and block height exceeds this value, the escrow is expired.
+    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    pub end_height: Option<u64>,
+    /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
+    /// block time exceeds this value, the escrow is expired.
+    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    pub end_time: Option<u64>,
+    /// Besides any possible tokens sent with the CreateMsg, this is a list of all cw20 token addresses
+    /// that are accepted by the escrow during a top-up. This is required to avoid a DoS attack by topping-up
+    /// with an invalid cw20 contract. See https://github.com/CosmWasm/cosmwasm-plus/issues/19
+    pub cw20_whitelist: Option<Vec<String>>,
+    },
+    ExpiredEscrow {
+    pub status: EscrowStatus,
+    /// id is a human-readable name for the escrow to use later
+    /// 3-20 bytes of utf-8 text
+    pub id: String,
+    /// arbiter can decide to approve or refund the escrow
+    pub arbiter: String,
+    /// if approved, funds go to the recipient
+    pub recipient: Option<String>,
+    /// Title of the escrow
+    pub title: String,
+    /// Longer description of the escrow, e.g. what conditions should be met
+    pub description: String,
+    /// When end height set and block height exceeds this value, the escrow is expired.
+    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    pub end_height: Option<u64>,
+    /// When end time (in seconds since epoch 00:00:00 UTC on 1 January 1970) is set and
+    /// block time exceeds this value, the escrow is expired.
+    /// Once an escrow is expired, it can be returned to the original funder (via "refund").
+    pub end_time: Option<u64>,
+    /// Besides any possible tokens sent with the CreateMsg, this is a list of all cw20 token addresses
+    /// that are accepted by the escrow during a top-up. This is required to avoid a DoS attack by topping-up
+    /// with an invalid cw20 contract. See https://github.com/CosmWasm/cosmwasm-plus/issues/19
+    pub cw20_whitelist: Option<Vec<String>>,
+    },
     /// This accepts a properly-encoded ReceiveMsg from a cw20 contract
     Receive(Cw20ReceiveMsg),
 }
@@ -47,6 +98,7 @@ pub enum ReceiveMsg {
 
 #[cw_serde]
 pub struct CreateMsg {
+    pub status: EscrowStatus,
     /// id is a human-readable name for the escrow to use later
     /// 3-20 bytes of utf-8 text
     pub id: String,
